3a6d9f3bc3c6226eb9cf419f406529cc7eb48c1c
Emanuele Guicciardi <eguicciardi@gmail.com>
2018-06-09 22:44:27 +0200

    Content serving structure
    - Cache
    - 404s
    - Express routes for static content
    - Directory structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# watchtower
-Watchtower is going to be an unopinionated little blog engine
+# Watchtower
+Watchtower is going to be an unopinionated little blog engine.
+
+# License
+Watchtower is MIT-Licensed. It's not required at all but I'll really appreciate a mention to this repository or [https://guicciardi.net](my website).
+
+# Change log
+
+**1.0.0** Initial release, it starts without so many errors
--- index.js
+++ index.js
@@ -1,3 +1,87 @@
-// Setting things up
+/* Setting things up */
+require('dotenv').load({ silent: true }); //Please take .ev.sample as reference
+
 var http = require('http');
 var express = require('express');
+
+var app = express();
+var server = http.createServer(app);
+
+/* Static things */
+var contentRoot = './content/'; // The folder where contents were stored in Markdown format
+
+var renderedContents = {}; // An array to store rendered content "keyed" by a normalized file name.
+
+/* Utilities */
+
+function normalizeFileName(file) {
+
+    var retVal = file;
+
+    retVal = retVal.replace('.md', '');
+
+    return retVal;
+
+}
+
+// Serving things
+function serve(file, response) {
+
+    if (chacheHit(file) !== NULL) {
+
+        // We have a cache hit so we serve the rendered content
+
+    } else {
+
+        // We render the content then we serve and cache it
+
+    }
+
+}
+
+// Checking for cached contents
+function chacheHit(file) {
+
+    return renderedContents[normalizeFileName(file)] || null;
+
+}
+
+// Sending 404s and love
+function send404(file, response) {
+
+    console.log('We have a 404 for: %s', file);
+    response.status(400).send('Oh no!');
+
+}
+
+/* Defining Express routes */
+
+app.get('/', (request, response) => response.send('Hello World!'));
+
+// Serving static pages
+app.get('/:slug', function(request, response) {
+
+    if (isNaN(request.params.slug)) {
+
+        var fileToBeServed = contentRoot + request.params.slug;
+        serve(fileToBeServed, response);
+
+    } else {
+
+        // Huston, we have a 404
+        send404(contentRoot + request.params.slug, response);
+
+    }
+
+});
+
+function init() {
+
+    console.log('Hello there!');
+
+}
+
+init();
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port, () => console.log('Watchtower is ready and listening on port %s!', server.address().port));
